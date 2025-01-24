5f908007ca871272b78abfb445f0330040c2a6cb
hatif11 <hatifmehdinaqvi@gmail.com>
2025-01-24 23:02:51 +0500

    new

---
+++ loops.py
@@ -0,0 +1,28 @@
+# for i in range(1,11):
+#   print(i)
+
+# fruits = ["apple", "banana", "cherry"]
+# for name in fruits:
+#   print(name)
+
+# character = "Codingal"
+# for char in character:
+#   print(char)
+
+# num = 6
+# while num <=  10:
+#   print(num)
+#   num += 1
+
+# while True:
+#  lööp = input ('if you want to stop the loop press "bye"')
+#  if lööp == 'bye':
+#    print('you have stopped the loop')
+#    break
+for i in range(1,6):
+  print('*' *i)
+
+for i in range(1,7):
+  for j in range(1,i+1):
+    print('#',end="")
+  print()
